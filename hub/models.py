97137f2502274964c0ec24e5a34ad6bb9927a2d5
vicct0r <v1ctor_inacio@proton.me>
2025-09-12 22:13:25 -0300

    feature (models): historico de transacoes

--- hub/models.py
+++ hub/models.py
@@ -30,10 +30,10 @@ class CD(Base):
 class Transaction(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     supplier = models.ManyToManyField(CD, verbose_name='sales') 
-    buyer = models.OneToOneField(CD, verbose_name='purchases')
+    buyer = models.ManyToManyField(CD, verbose_name='purchases')
     product = models.CharField(max_length=100)
     quantity = models.IntegerField()
     total = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self):
-        return f"{self.created_at:%Y-%m-%d %H:%M} | Sale: {self.supplier} -> Purchase: {self.buyer} | Total: {self.total}"
+        return f"{self.created_at:%Y-%m-%d %H:%M} | Sale: {self.supplier} - Purchase: {self.buyer} | Total: {self.total}"
