1df89b06d41b7b9f925a626dd02895eb86d52cd2
vicct0r <v1ctor_inacio@proton.me>
2026-01-25 15:41:57 -0300

    refactor: client and trade operations refactor flow v1.

--- hub/views.py
+++ hub/views.py
@@ -2,103 +2,43 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
-from django.db import transaction
 
-import requests
-
-from .serializers import FullCDSerializer, RequestCDSerializer, TransactionSerializer
-from .models import CD, Transaction
+from .serializers import FullClientSerializer
+from .models import CD
 
 
-class CdListCreateAPIView(generics.ListCreateAPIView):
+class ClientCreateAPIView(generics.CreateAPIView):
+    serializer_class = FullClientSerializer
+    queryset = CD.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response({
+            "status": "success",
+            "operation": "service_creation",
+            "client": response.data
+        }, status=response.status_code)
+
+
+class ClientListAPIView(generics.ListAPIView):
     """
-    **POST** Distribution Center **creation** objects endpoint
-    - **Full serializer** with models.Base fields
+    **POST** Distribution Center **creation** objects endp
+    
     """
     queryset = CD.objects.all()
-    serializer_class = FullCDSerializer
+    serializer_class = FullClientSerializer
 
     def get_queryset(self):
         return CD.objects.all().order_by("id")
 
+
 class CdRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = CD.objects.all()
-    serializer_class = FullCDSerializer
+    serializer_class = FullClientSerializer
 
     def get_object(self):
         if self.kwargs.get('id'):
             return get_object_or_404(self.queryset, id=self.kwargs.get('id'))
         elif self.kwargs.get('slug'):
             return get_object_or_404(self.queryset, slug=self.kwargs.get('slug'))
-
-
-class CdRequestAPIView(APIView):
-    """
-    **POST** - Transaction trade for Distribution Centers
-    - CDs trade products trough this endpoint
-    - Buyer CD will request this endpoint to ask HUB for more products
-    - HUB will gather all candidates, choosing the cheaper one
-    - HUB validates and operate the trade between the CD actors
-    """
-    def post(self, request, *args, **kwargs):
-        serializer = RequestCDSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        data = serializer.validated_data
-
-        product = data['product']
-        quantity = data['quantity']
-        ip = data['ip']
-
-        supliers = {}
-        available_supliers = CD.objects.exclude(ip=ip)
-
-        try:
-            for unity in available_supliers:
-                
-                if unity.ip == ip:
-                    continue
-
-                response = requests.get(
-                url = f"http://{unity.ip}/cd/v1/product/request/{product}/{quantity}/",
-                timeout=5
-                )
-
-                response.raise_for_status()
-
-                if response.status_code != 200:
-                    continue
-
-                _data = response.json()
-
-                supliers[str(unity.id)] = {
-                    "ip": unity.ip,
-                    "id": _data["id"],
-                    "total_price": _data["total_price"]
-                }
-
-        except Exception as e:
-            return Response({
-                "status": "success",
-                "error_msg": f"{str(e)}"
-            }, status=status.HTTP_400_BAD_REQUEST)
-        
-        return Response({
-            "status": "code",
-            "supliers": supliers,
-            "action": "report"
-        }, status=status.HTTP_200_OK)        
-
-
-class TransactionsListAPIView(generics.ListAPIView):
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
-
-    def get_queryset(self):
-        return Transaction.objects.all().order_by("id")
-
-
-class TransactionRetrieveAPIView(generics.RetrieveAPIView):
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
-    lookup_field = 'id'
-
+     
