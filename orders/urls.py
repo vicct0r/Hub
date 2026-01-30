32445dc672bd1b073563daf608dafeaf38db6976
vicct0r <v1ctor_inacio@proton.me>
2026-01-30 12:20:58 -0300

    .

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.OrderListAPIView.as_view()),
+    path('', views.OrderListCreateAPIView.as_view()),
     path('<uuid:id>/', views.OrderRetrieveAPIView.as_view(), name='order_detail'),
     path('request/', views.OrderCreateAPIView.as_view()),
     path('o/<uuid:id>/', views.OrderRetrieveUpdateDeleteAPIView.as_view()),
