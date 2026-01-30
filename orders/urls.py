6da23668d76717043375311a9c61e7df062a8685
vicct0r <v1ctor_inacio@proton.me>
2026-01-30 12:22:09 -0300

    .v2

--- orders/urls.py
+++ orders/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.OrderListCreateAPIView.as_view()),
     path('<uuid:id>/', views.OrderRetrieveAPIView.as_view(), name='order_detail'),
-    path('request/', views.OrderCreateAPIView.as_view()),
     path('o/<uuid:id>/', views.OrderRetrieveUpdateDeleteAPIView.as_view()),
 ]
